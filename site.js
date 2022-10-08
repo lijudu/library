f21b047fdbc18131724e6b0304347b304199bc57
Lisa <mkoliju@gmail.com>
2022-10-07 20:24:28 -0700

    JS add new book info

--- site.js
+++ site.js
@@ -1,5 +1,9 @@
 
-let myLibrary = [];
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'read');
+const dune = new Book ('Dune', 'Frank Hebert', 'read');
+const kafka = new Book('Kafka on the Shore', 'Haruki Murakami', 'read');
+
+let myLibrary = [hobbit, dune, kafka];
 
 function Book(title, author, status) {
     this.title = title
@@ -10,15 +14,38 @@ function Book(title, author, status) {
     }
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'read');
-const dune = new Book ('Dune', 'Frank Hebert', 'read');
-const kafka = new Book('Kafka on the Shore', 'Haruki Murakami', 'read');
-
 function addArray(Book){
     myLibrary.push(Book);
     console.log(myLibrary);
 }
 
+const titleContainer = document.getElementById("bookName")
+const authorContainer = document.getElementById("authorName")
+
+const newBook = document.createElement("p")
+const newBook2 = document.createElement("p")
+const newAuthor = document.createElement("p")
+
+// let newEntry = document.createTextNode(myLibrary[1].title);
+newBook.innerText = myLibrary[1].title
+newAuthor.innerText = myLibrary[1].author;
+newBook2.innerText = myLibrary[2].title;
+
+
 function addBookToLibrary() {
-    // do stuff here
+    titleContainer.appendChild(newBook)
+    titleContainer.appendChild(newBook2)
+    authorContainer.appendChild(newAuthor)
 }
+
+
+addBookToLibrary(myLibrary)
+
+
+
+
+// 1) Create new row when "submit" clicked
+// 2) loop through myLibrary and add information (title=column1, author=column2, read=column3, add deleteBTN)
+// 3) when "submit" clicked, add to myLibrary 
+// 4) delete button 
+// 5) change read status to toggle 
