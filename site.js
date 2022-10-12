219826c3c069178af383eb70224c4b0bd92ec064
Lisa <mkoliju@gmail.com>
2022-10-12 13:13:01 -0700

    Create myLibrary array

--- site.js
+++ site.js
@@ -3,7 +3,9 @@ const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'read');
 const dune = new Book ('Dune', 'Frank Hebert', 'read');
 const kafka = new Book('Kafka on the Shore', 'Haruki Murakami', 'read');
 
-let myLibrary = [hobbit, dune, kafka];
+let testarr = [dune,kafka]
+
+let myLibrary = [hobbit];
 
 function Book(title, author, status) {
     this.title = title
@@ -14,9 +16,12 @@ function Book(title, author, status) {
     }
 }
 
-function addArray(){
-
+function addArray(book){
+    myLibrary.push(book)
 }
+
+testarr.forEach((book) => addArray(book));
+
 
 const container = document.getElementById('libraryContainer')
 const submitBTN = document.getElementById('submit')
@@ -28,8 +33,31 @@ function createCard(){
     const newDiv = document.createElement('div')
     container.appendChild(newDiv)
     newDiv.classList.add('newCard')
+    const newTitle = document.createElement('p')
+    const newAuthor = document.createElement('p')
+    const readStatus = document.createElement('button')
+    const delBook = document.createElement('button')
+    const lineBreak = document.createElement('br')
+    readStatus.id='readBTN'
+    delBook.id= 'deleteBTN'
+    newDiv.appendChild(newTitle)
+    newDiv.appendChild(newAuthor)
+    newDiv.appendChild(readStatus)
+    newDiv.appendChild(lineBreak)
+    newDiv.appendChild(delBook)
+    readBTN = document.getElementById('readBTN')
+    deleteBTN = document.getElementById('deleteBTN')
+    readShow = document.createTextNode('Read')
+    delShow = document.createTextNode('Delete')
     // fill out info in Card (title, author, read, delete)
-    const newCard = document.querySelectorAll('.newCard')
+    let sampleTitle = myLibrary[1].title
+    let sampleAuthor = myLibrary[1].author
+    newTitle.innerText = sampleTitle
+    newAuthor.innerText = sampleAuthor
+    readStatus.appendChild(readShow)
+    delBook.appendChild(delShow)
+
+
 }
 
 
